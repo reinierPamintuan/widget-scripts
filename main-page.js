f7d474b61715fd6b32fe2e18e9248744435a1d51
Reinier Pamintuan <reinier.pamintuan@gmail.com>
2022-08-17 00:55:35 -0700

    Replaced placeholder red with transparent back

--- main-page.js
+++ main-page.js
@@ -1,10 +1,27 @@
 function createWidget() {
     let widget = new ListWidget();
-    widget.backgroundColor = Color.black();
-    widget.addText('hello!');
+    const files = FileManager.iCloud();
+    const fileName = "/Transparent_back2.JPG";
+    //const path = files.joinPath(files.documentsDirectory(),fileName)
+    const path = files.documentsDirectory();
+
+
+    const cloudFileName = "/.Transparent_back2.JPG.icloud";
+    if(!files.isFileDownloaded(path+cloudFileName)){
+        files.downloadFileFromiCloud(path+cloudFileName);
+    }
+    
+    
+    
+    widget.backgroundImage = files.readImage(path+fileName);
+    widget.addText(path);
     widget.addSpacer();
+    console.log(files.listContents(path));
     return widget;
 }
+
+
+//IMG_6566
 
 let mainWidget = createWidget();
 Script.setWidget(mainWidget);
